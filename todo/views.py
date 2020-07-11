b86f98b93660adf97008f2a79effca01fdf17bfd
sroopsai <roopsai.work@gmail.com>
2020-07-11 19:57:02 +0200

    Add todo_retrive functionality

--- todo/views.py
+++ todo/views.py
@@ -11,3 +11,10 @@ def todo_list(request):
         "todos_list": todos
     }
     return render(request, "todo/todo_list.html", context)
+
+def todo_retrieve(request, id):
+    todo = Todo.objects.get(id)
+    context = {
+        "todo": todo
+    }
+    return render(request, "todo/todo_detail.html", context)
