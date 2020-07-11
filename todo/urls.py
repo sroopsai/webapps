ea0d275732910e2dcdceccbbf9cc4f324c0ecd5c
sroopsai <roopsai.work@gmail.com>
2020-07-11 23:43:27 +0200

    Change path rule for todo app

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,6 @@ from .views import todo_list, todo_retrieve
 app_name = 'todos'
 
 urlpatterns = [
-    path('', todo_list),
-    path('<id:int>/', todo_retrieve)
+    path('todo_list/', todo_list),
+    path('todo_list/<id>/', todo_retrieve)
 ]
