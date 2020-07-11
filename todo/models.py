94e24673aae574cb75eaf4670b38d87d3a57155d
sroopsai <roopsai.work@gmail.com>
2020-07-11 18:24:47 +0200

    Add Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=120)
+    due_time = models.TimeField()
+    due_date = models.DateField()
