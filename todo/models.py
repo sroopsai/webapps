cca3b33fea6ce62592ade037e792a3d2ed899b28
sroopsai <roopsai.work@gmail.com>
2020-07-11 18:36:21 +0200

    Add string representation for model Todo

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,6 @@ class Todo(models.Model):
     name = models.CharField(max_length=120)
     due_time = models.TimeField()
     due_date = models.DateField()
+
+    def __str__(self):
+        return self.name
