177f57ef062c4811255b7a8eade51126eb612cfb
sroopsai <roopsai.work@gmail.com>
2020-07-11 19:58:45 +0200

    Add retrieve url

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list, todo_retrieve
 
 app_name = 'todos'
 
 urlpatterns = [
-    path('', todo_list)
+    path('', todo_list),
+    path('<id:int>/', todo_retrieve)
 ]
