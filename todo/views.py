7d6e5c839563d352fcad3ed227b99acd8ecf23c5
sroopsai <roopsai.work@gmail.com>
2020-07-12 23:45:50 +0200

    Add code to handle form saving

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import TodoForm
 from todo.models import Todo
 
 
@@ -19,3 +19,17 @@ def todo_retrieve(request, id):
         "todo": todo
     }
     return render(request, "todo/todo_retrieve.html", context)
+
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        # create a Todo object
+        name = form.cleaned_data['name']
+        due_date = form.cleaned_data['due_date']
+        due_time = form.cleaned_data['due_time']
+        Todo.objects.create(name=name, due_date=due_date, due_time=due_time)
+        return redirect('/')
+
+    context = {"form": form}
+    return render(request, "todo/todo_create.html", context)
