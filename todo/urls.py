0d7c4813ffe8c9d4dbc89663870251dc0de191a1
sroopsai <roopsai.work@gmail.com>
2020-07-12 23:26:18 +0200

    Add path to create view

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import todo_list, todo_retrieve
+from .views import todo_list, todo_retrieve, todo_create
 
 app_name = 'todos'
 
 urlpatterns = [
     path('todo_list/', todo_list),
+    path('todo_list/create_todo', todo_create),
     path('todo_list/<id>/', todo_retrieve)
 ]
