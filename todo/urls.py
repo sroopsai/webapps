8c1fb263512d072f3f968c79bf954d2ce9aad866
sroopsai <roopsai.work@gmail.com>
2020-07-13 22:03:13 +0200

    Add url paths for todo_update and todo_delete

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import todo_list, todo_retrieve, todo_create
+from .views import (
+    todo_list,
+    todo_retrieve,
+    todo_create,
+    todo_update,
+    todo_delete
+)
 
 app_name = 'todos'
 
 urlpatterns = [
     path('todo_list/', todo_list),
     path('todo_list/create_todo', todo_create),
-    path('todo_list/<id>/', todo_retrieve)
+    path('todo_list/<id>/', todo_retrieve),
+    path('todo_list/<id>/update_todo', todo_update),
+    path('todo_list/<id>/delete_todo', todo_delete)
 ]
