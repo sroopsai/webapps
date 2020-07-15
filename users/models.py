f7ed33e0747d0dd9ef0e31bd87be0d57549a336f
sroopsai <roopsai.work@gmail.com>
2020-07-15 22:38:29 +0200

    Add WebApps model

--- users/models.py
+++ users/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+
+class WebApps(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
