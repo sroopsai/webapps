d361c268e20c1ee8d6d86bfe26a8f31f75928c23
sroopsai <roopsai.work@gmail.com>
2020-07-15 22:52:36 +0200

    Add status field to WebApps model

--- users/models.py
+++ users/models.py
@@ -7,9 +7,18 @@ class User(models.Model):
     last_name = models.CharField(max_length=30)
 
 
+STATUS_CHOICES = (
+    ('R', 'Reviewed'),
+    ('N', 'Not Reviewed'),
+    ('A', 'Accepted'),
+    ('E', 'Error')
+)
+
+
 class WebApps(models.Model):
     name = models.CharField(max_length=50)
     url = models.URLField()
     release_date = models.DateField()
     rating = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
