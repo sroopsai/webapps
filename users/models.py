0198b6d30671ac8b006ebd5f8b2a3f9447786732
sroopsai <roopsai.work@gmail.com>
2020-07-16 23:27:46 +0200

    Add verbose naming

--- users/models.py
+++ users/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField("the user's first name", max_length=30)
+    last_name = models.CharField("the user's last name", max_length=30)
+    cars = models.ManyToManyField('Car', verbose_name="the user's cars")
+
+    def __str__(self):
+        return self.first_name + self.last_name
 
 
 STATUS_CHOICES = (
@@ -17,8 +21,12 @@ STATUS_CHOICES = (
 
 class WebApps(models.Model):
     name = models.CharField(max_length=50)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     release_date = models.DateField()
     rating = models.IntegerField()
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
